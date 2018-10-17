15a28bbaef6bc0fc1517e05769827cfec38c524d
Jacob Lindberg <34181945+jacolind@users.noreply.github.com>
2018-10-17 15:13:10 +0200

    Update bandwitdh.py
    Add A and B as parameters. Add 60/40 tolerance rebalanced TRD. Then BLX & TRD becomes PORTF.

--- bandwitdh.py
+++ bandwitdh.py
@@ -3,40 +3,51 @@
 
 ## define invest and rebalance functions 
 
-def invest(df, i, amount, p):
+def invest(df, i, amount, p, A, B, rm_shares=False):
     """
-    Invest amount dollars between P% BLX and 1-P% TRD,
-    starting at ordinal index i.
+    Invest a USD amount in p% of asset A and 1-p% in asset B,
+    starting at ordinal index i,
+    where A and B are the names of the assets and these cols contain prices!
     This modifies df, by creating new cols: shares & value & ratio.
+    col with shares can be removed. 
     """
     c = dict([(col, j) for j, col in enumerate(df.columns)])
     
     # prices are from the market, but the nr of shares to own we cancontrol
     
     # 1) reset nr shares of shares you own 
-    # shares = value / share price. 
+    # shares bought = amount invested / share price. 
     # we allow for fractions of shares. 
-    value_blx = amount * p
-    value_trd = amount * (1-p)
-    df.iloc[i:, c['BLX_shares']] = value_blx / df.iloc[i, c['BLX']]
-    df.iloc[i:, c['TRD_shares']] = value_trd / df.iloc[i, c['TRD']]
+    amount_A = amount * p
+    amount_B = amount * (1-p)
+    A_shares = A + '_shares'
+    B_shares = B + '_shares'
+    df.iloc[i:, c[A_shares]] = amount_A / df.iloc[i, c[A]]
+    df.iloc[i:, c[B_shares]] = amount_B / df.iloc[i, c[B]]
     
     # 2) based on shares owned being reset, calc what value those are by
-    # value = share price * shares 
-    df.iloc[i:, c['BLX_value']] = (df.iloc[i:, c['BLX']] * 
-                                   df.iloc[i:, c['BLX_shares']])
-    df.iloc[i:, c['TRD_value']] = (df.iloc[i:, c['TRD']] * 
-                                   df.iloc[i:, c['TRD_shares']])
+    # value = share price * shares bought 
+    A_value = A + '_value'
+    B_value = B + '_value'
+    df.iloc[i:, c[A_value]] = (df.iloc[i:, c[A]] * 
+                                   df.iloc[i:, c[A_shares]])
+    df.iloc[i:, c[B_value]] = (df.iloc[i:, c[B]] * 
+                                   df.iloc[i:, c[B_shares]])
     
     # 3) set ratio of how much value we have in each asset 
-    df.iloc[i:, c['ratio']] = (df.iloc[i:, c['BLX_value']] / 
-                               df.iloc[i:, c['TRD_value']]+df.iloc[i:, c['BLX_value']])
+    df.iloc[i:, c['ratio']] = (df.iloc[i:, c[A_value]] / 
+                               df.iloc[i:, c[A_value]]+df.iloc[i:, c[B_value]])
+                               
+    # remove cols 
+    if rm_shares:
+        del df[A_shares]
+        del df[B_shares]
 
 # todo: could make the function more general by accepting input
-# A='BLX' B='TRD'. then we could reuse the invest() to create a 60/40 portoflio 
+# A='BLX' B='B'TRD'. then we could reuse the invest() to create a 60/40 portoflio 
 # based on only two column namely prices of stocks and bonds. 
 
-def rebalance(df, tol, p, i=0):
+def rebalance(df, tol, p, i=0, rm_shares=False, rm_cols=False):
     """
     Rebalance df whenever the ratio falls outside the tolerance range.
     This modifies df, by adjusting the cols shares & value, based on ratio.
@@ -62,32 +73,56 @@ def rebalance(df, tol, p, i=0):
         # 3) at that location, re-invest (i.e. rebalance)
         
         # amount we have in our porftolio now 
-        amount = (df.iloc[i, c['BLX_value']] + df.iloc[i, c['TRD_value']])
+        amount = (df.iloc[i, c[A_value]] + df.iloc[i, c[B_value]])
         # invest that amount (this begins investing process again e.g. rebalnce)
-        invest(df, i, amount, p)
+        invest(df, i, amount, p, rm_shares)
+    # remove cols 
+    if rm_cols:
+      # recall A shares is taken care of otherwise 
+      cols_created = ['ratio', A_value, B_value]
+      df.drop(cols_created, inplace=True)
     return df
 
 ## create our bandwidth rebalanced portfolio 
 
-# set ideal weight for BLX 
-w_target = 0.05
+# firstly, create TRD: a 60%/40% stocks/bonds portfolio. tolereance band 5%. 
+invest(df, i=0, amount=100, p=0.60, A='Stocks', B='Bonds', rm_shares=True)
+rebalance(df, tol=0.05, p=0.60, A='Stocks', B='Bonds', rm_shares=True)
+# invest() create cols and rebalance() change their values by rebalancing.
+# based on new columns that wes create, create portfolio value 
+df['TRD'] = df['Stocks_value'] + df['Bonds_value]
+# delete or rename ratio since it will be used later
+del df['ratio'] 
 
-# set how many percentage points BLX is allowed to be outside ideal weight
+# seconldy, create a 5%/95% portfolio of BLX/TRD 
+invest(df, i=0, amount=100, p=w_target, A='BLX', B='TRD')
+w_target = 0.05 
 w_band = 0.02
-
-# assuming df contains two cols df contain two cols: TRD and BLX with prices.
-# below till return df with new columns. 
-invest(df, i=0, amount=100, p=w_target) # create cols  
-rebalance(df, tol=w_band, p=w_target) # change value of cols 
-
-# based on new column, create portfolio value 
-df['PORTF_value'] = df['BLX_value'] + df['TRD_value']
+rebalance(df, tol=w_band, p=w_target, A='BLX', B='TRD') 
+# create portfolio value for PORT: 5%/95%
+df['PORTF_value'] = df['BLX'] + df['TRD']
 df['BLX_weight'] = df['BLX_value'] / df['PORTF_value']
 df['TRD_weight'] = df['TRD_value'] / df['PORTF_value']
+# delete cols we do not need 
+df.drop(['BLX_shares', 'TRD_shares', 
+         'BLX_value', 'TRD_value'], inplace=True)
+
+## see the portfolio 
+
+# plot value over time 
+df[['PORT_value', 'BLX', 'TRD']].plot()
+
+# see all weights over time 
+print(df['BLX_weight'])
+
+# see how often a sell is required 
+print(df['ratio'] >= 1+w_band)
+# see how often a buy is required 
+print(df['ratio'] =< 1-w_band)
 
 # show the rows which trigger rebalancing
-mask = (df['ratio'] >= w_target+w_band) | (df['ratio'] <= w_target-w_band)
-print(df.loc[mask])
+mask_1 = (df['ratio'] >= w_target+w_band) | (df['ratio'] <= w_target-w_band)
+print(df.loc[mask_1])
 
 # see what the max and min weights are 
 # they can be outside the range, but only for one time period. 
@@ -97,11 +132,3 @@ print(df['BLX_weight'].min())
 print(df['BLX_weight'].max())
 print(df['TRD_weight'].min())
 print(df['TRD_weight'].max())
-
-# see all weights 
-print(df['BLX_weight'])
-
-# see how often a sell is required 
-print(df['ratio'] >= 1+w_band)
-# see how often a buy is required 
-print(df['ratio'] =< 1-w_band)
