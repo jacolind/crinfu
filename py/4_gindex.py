a1d389127aaed73b33bc5a0da386d036e10df5e4
Jacob Lindberg <34181945+jacolind@users.noreply.github.com>
2018-11-15 11:26:33 +0100

    Create 4_gindex.py
    add purpose of file

---
+++ py/4_gindex.py
@@ -0,0 +1,14 @@
+"""
+let us do more functions and less crating objects!
+
+currently i do this:
+import data from web or long.csv
+def transform function 
+tranform to new objects
+check transform is ok and export to new objects 
+
+now i want to do this 
+import data from web or long.csv 
+def function that completes transform, check it is ok, and export to new object. all in one script. 
+transform to new objects and update the objects themselves, using classes (eg an index class).
+"""
