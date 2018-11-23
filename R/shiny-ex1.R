bfeb591346717c8c231370a7e035be67ebb8151e
Jacob Lindberg <34181945+jacolind@users.noreply.github.com>
2018-11-23 17:14:50 +0100

    Update shiny-ex1.R

--- R/shiny-ex1.R
+++ R/shiny-ex1.R
@@ -1,5 +1,6 @@
 # similar to https://shiny.rstudio.com/tutorial/written-tutorial/lesson6/
 
+# same ui as before 
 ui <- fluidPage(
   selectInput("assets",
               label = "type in all names you want. autocomplete ON",
@@ -14,7 +15,7 @@ ui <- fluidPage(
                  min =  "2014-01-01",  
                  max = "2018-06-06" 
   ),
-  actionButton("done", "click to show result"),
+  actionButton("click", "click to show plot."),
   # output 
   plotOutput("plot"),
   tableOutput("tbl")
@@ -22,10 +23,7 @@ ui <- fluidPage(
 
 # server 
 server <- function(input, output){
-  # df_3 <- df %>%
-  #     filter(symbol %in% input$assets) %>%
-  #     select(symbol, mcap) %>%
-  #     head()
+  # calc data, reactive 
   data <- reactive({
     df_plot <- df %>%
       filter(symbol %in% input$assets) %>%
@@ -33,13 +31,24 @@ server <- function(input, output){
              date < input$dates[2]) %>%
       select(date, symbol, mcap)
     })
+  
+  # table 
   output$tbl <- renderTable(
     data() %>% 
-    group_by(symbol) %>%
-    summarise(mean(mcap)/10^9))
-  output$plot <- renderPlot(
-    ggplot(data(), aes(x=date, y=mcap, color=symbol)) + 
-      geom_line()
+    group_by(symbol) %>% 
+    summarise(`mean marketcap in $bn` = mean(mcap)/10^9))
+  
+  # plot and button 
+  observeEvent(input$click,
+    output$plot <- renderPlot(
+      ggplot(data(), aes(x=date, y=mcap, color=symbol)) + 
+        geom_line(lwd=1) +
+        scale_y_log10() + 
+        annotation_logticks() +
+        labs(title="Market capitalization", 
+             y="Market capitalization \n in USD billion \n log scale") +
+        theme_minimal()
+    )
   )
 }
 
